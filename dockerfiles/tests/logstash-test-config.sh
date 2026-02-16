971473d5076abffbb78486f012961e38334d37f7
Ben Wilson <doubtingben@gmail.com>
2026-02-15 21:19:30 -0500

    delete old files

--- dockerfiles/tests/logstash-test-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-docker run -v $(pwd)../logstash/pipeline/:/usr/share/logstash/pipeline/ --rm -ti backchannel_logstash logstash --config.test_and_exit
--- producer/replicas.sh
+++
@@ -1,1 +0,0 @@
-while true; do curl -vX POST http://127.0.0.1:9400 -d @./replicas.json --header "Content-Type: application/json"; sleep 1;done
